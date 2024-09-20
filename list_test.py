4299d76bda54cc4d7bf28057a2dea3674378ecab
Divya Udayakumar <divyaudayakumar47@gmail.com>
2024-09-20 09:35:57 -0700

    lists in python

---
+++ list_test.py
@@ -0,0 +1,8 @@
+#used to store a bounch of values
+friends=["Maya","Meena",2,"Jaya"] #one list can contain multiple type(str,int.float) of items
+print(friends) #print the entire list
+print(friends[0]) #print the first item
+print(friends[-1]) #print the last item
+
+print(friends[1:]) #print the second and other items after that
+print(friends[1:3])#print the 2nd and 3rd items
