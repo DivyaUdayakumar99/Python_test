7635bbd7a11122586f12931e25102db09e06d63e
Divya Udayakumar <divyaudayakumar47@gmail.com>
2024-09-28 08:14:12 -0700

    loops

--- for_loop.py
+++ for_loop.py
@@ -11,3 +11,13 @@ for index1 in range(3,10):#print from 3 to 9
 print("next loop")
 for index2 in range(10,2,-3):#print from 3 to 9
     print(index2)
+
+def raise_power(basenum,pownum):
+    print("your inside function")
+    result=1
+    for index in range(pownum):
+        print(index)
+        result=result * basenum
+    return (result)
+
+print(raise_power(2,5))
