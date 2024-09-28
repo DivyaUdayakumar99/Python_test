f921311688a6b45bb3f0e5f74e1b6ca7234d0c60
Divya Udayakumar <divyaudayakumar47@gmail.com>
2024-09-28 07:52:19 -0700

    loops

---
+++ for_loop.py
@@ -0,0 +1,13 @@
+for letter in "My files":
+    print(letter)
+for words in "MY Dady is my hero":
+    print(words)
+#using for loop in lists
+friends3=["Maya","Meena","Mini"]
+for index in range(10):
+    print(index)
+for index1 in range(3,10):#print from 3 to 9
+    print(index1)
+print("next loop")
+for index2 in range(10,2,-3):#print from 3 to 9
+    print(index2)
